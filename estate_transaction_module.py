6b7c320d8bf3550bddd4f151c09820ea22128101
donggyunwoo <donggyunwoo@kr.kpmg.com>
2022-06-20 00:05:40 +0900

    도로명 주소 api 건물정보 api 연동

--- estate_transaction_module.py
+++ estate_transaction_module.py
@@ -1,7 +1,5 @@
-from audioop import add
 from pprint import pprint
 import requests, json
-import pandas as pd
 from bs4 import BeautifulSoup as bs
 import lxml
 import aiohttp, asyncio
@@ -49,14 +47,14 @@ def get_building_info(response: list):
 
 async def request_building_info(pnu:str):
     url = 'http://apis.data.go.kr/1611000/nsdi/BuildingUseService/attr/getBuildingUse'
-    params ={'serviceKey' : f'{key}', 'pageNo' : '1', 'numOfRows' : '40', 'pnu' : f'{pnu}', 'format' : 'json' }
+    params = {'serviceKey' : f'{key}', 'pageNo' : '1', 'numOfRows' : '40', 'pnu' : f'{pnu}', 'format' : 'json' }
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url=url, params=params) as response:
                 res = await response.json()
         res = res.get('buildingUses')
         if not res or res.get('totalCount')=='0' or res.get('totalCount') is None:
-            raise Exception(f'건물정보 없거나 에러')
+            raise Exception(f'error : {pnu} : 건물정보 없거나 에러')
         info = get_building_info(res.get('field'))
         
     except Exception as e:
@@ -69,6 +67,9 @@ async def main_test(test_case: list):
     list_code = await asyncio.gather(*[request_doro_api(addr) for addr in testcase])
     for addr_code in list_code:
         if not addr_code[0]: continue
+    await asyncio.gather(*[request_building_info(pnu[1]) for pnu in list_code])
+
+
         
     return list_code
 
@@ -77,5 +78,83 @@ async def main_test(test_case: list):
 
 if __name__ == '__main__':
     # testcase = ['서울 중구 동호로24길 27-13', '장충동2가 211', '서울시 강동구 암사동 513-29','충북 청주시 흥덕구 대농로 55']
-    testcase = ['서울시 강남구 역삼동 152', '서울 강남 역삼동 737']
+    testcase = ['서울 강남 역삼동 737', '장충동2가 211','서울시 송파구 송파대로 345','충북 청주시 흥덕구 대농로 55', '충북 청주시 흥덕구 복대동 2397']
     r = asyncio.run(main_test(test_case=testcase))
+'''
+def request_building_info
+error : 1114014400101860044 : 건물정보 없거나 에러
+def request_building_info
+error : 1171010700104790000 : 건물정보 없거나 에러
+def request_building_info
+error : 4311311400102880014 : 건물정보 없거나 에러
+{'agbldgSeCode': '1',
+ 'agbldgSeCodeNm': '일반건축물',
+ 'btlRt': '42.57',
+ 'buldBildngAr': '5600.51',
+ 'buldDongNm': None,
+ 'buldHg': '202.65',
+ 'buldIdntfcNo': '12777',
+ 'buldKndCode': '2',
+ 'buldKndCodeNm': '일반건축물대방',
+ 'buldMainAtachSeCode': '0',
+ 'buldMainAtachSeCodeNm': '주건축물',
+ 'buldNm': '강남파이낸스센터',
+ 'buldPlotAr': '13156.7',
+ 'buldPrposClCode': '2',
+ 'buldPrposClCodeNm': '상업용',
+ 'buldTotar': '212615.29',
+ 'detailPrposCode': '14299',
+ 'detailPrposCodeNm': '기타일반업무시설',
+ 'gisIdntfcNo': '2001203158584442021200000000',
+ 'groundFloorCo': '45',
+ 'lastUpdtDt': '2022-02-23',
+ 'ldCode': '1168010100',
+ 'ldCodeNm': '서울특별시 강남구 역삼동',
+ 'mainPrposCode': '14000',
+ 'mainPrposCodeNm': '업무시설',
+ 'measrmtRt': '995.19',
+ 'mnnmSlno': '737',
+ 'pnu': '1168010100107370000',
+ 'prmisnDe': '1995-05-04',
+ 'regstrSeCode': '1',
+ 'regstrSeCodeNm': '일반',
+ 'strctCode': '42',
+ 'strctCodeNm': '철골철근콘크리트구조',
+ 'undgrndFloorCo': '8',
+ 'useConfmDe': '2001-07-31'}
+{'agbldgSeCode': '1',
+ 'agbldgSeCodeNm': '일반건축물',
+ 'btlRt': '0',
+ 'buldBildngAr': '122.32',
+ 'buldDongNm': None,
+ 'buldHg': '0',
+ 'buldIdntfcNo': '126117',
+ 'buldKndCode': '2',
+ 'buldKndCodeNm': '일반건축물대방',
+ 'buldMainAtachSeCode': '0',
+ 'buldMainAtachSeCodeNm': '주건축물',
+ 'buldNm': None,
+ 'buldPlotAr': '0',
+ 'buldPrposClCode': '1',
+ 'buldPrposClCodeNm': '주거용',
+ 'buldTotar': '489.28',
+ 'detailPrposCode': '01003',
+ 'detailPrposCodeNm': '다가구주택',
+ 'gisIdntfcNo': '1991240067273471562200000000',
+ 'groundFloorCo': '3',
+ 'lastUpdtDt': '2022-02-23',
+ 'ldCode': '4311311400',
+ 'ldCodeNm': '충청북도 청주시 흥덕구 복대동',
+ 'mainPrposCode': '01000',
+ 'mainPrposCodeNm': '단독주택',
+ 'measrmtRt': '0',
+ 'mnnmSlno': '2397',
+ 'pnu': '4311311400123970000',
+ 'prmisnDe': '1991-12-09',
+ 'regstrSeCode': '1',
+ 'regstrSeCodeNm': '일반',
+ 'strctCode': '21',
+ 'strctCodeNm': '철근콘크리트구조',
+ 'undgrndFloorCo': '1',
+ 'useConfmDe': '1991-07-22'}
+'''
